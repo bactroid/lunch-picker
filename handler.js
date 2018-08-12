f60c77bf27c14176cd2476763b405b1a88bd74f7
Kate Haskell <fuzzcat@bactroid.net>
2018-08-12 13:49:21 -0400

    Added error handling to handler

--- handler.js
+++ handler.js
@@ -2,6 +2,8 @@ const {composeP} = require('ramda')
 const {getRestaurants} = require('./src/db')
 const {getRandomRestaurant} = require('./src/picker')
 
+const errorMessage = 'I\'m having trouble. Maybe just go for coffee?'
+
 const makeReply = text => ({
   statusCode: 200,
   body: JSON.stringify({
@@ -17,5 +19,9 @@ module.exports.lunchpicker = (event, context, callback) => {
   selectRandomRestaurantFromTable(process.env.LUNCH_TABLE)
     .then(restaurant => {
       callback(null, makeReply(restaurant.name))
+    })
+    .catch(err => {
+      console.error(err)
+      callback(null, makeReply(errorMessage))
     })
 }
